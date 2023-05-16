0c94ccc31d4bf22c1834c0f92112e0b9016c1747
ayoub78170 <96531528+ayoub78170@users.noreply.github.com>
2023-05-16 09:31:23 +0200

    Add files via upload

---
+++ js/disney-qf.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
